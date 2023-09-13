e602a5b261a097d109c31ed8907a0fc8fec06af9
SITEB\a.kianoor <ask1366@yahoo.com>
2023-09-13 15:19:56 +0330

    Update Kafka Placement

---
+++ src/Daryaftyar.Infrastructure/Kafka/ConsumerWrapper.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace Daryaftyar.Infrastructure.Kafka
+{
+    public class ConsumerWrapper
+    {
+        private readonly ILogger<ConsumerWrapper> _logger;
+        private readonly IConsumer<string, string> _consumer;
+
+        public ConsumerWrapper(ILogger<ConsumerWrapper> logger,
+                               ConsumerConfig config,
+                               string topicName)
+        {
+            _logger = logger;
+            _consumer = new ConsumerBuilder<string, string>(config)
+              .SetErrorHandler((_, e) => _logger.LogError(e.Reason))
+              .Build();
+
+            _consumer.Subscribe(topicName);
+        }
+
+        public string ReadMessages(CancellationToken cancellationToken)
+        {
+            try
+            {
+                ConsumeResult<string, string> result = new();
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+
+                    try
+                    {
+                        result = _consumer.Consume(cancellationToken);
+                    }
+                    catch (ConsumeException exc)
+                    {
+                        _logger.LogError(exc.Error.Reason);
+                        continue;
+                    }
+
+                }
+
+                return result.Message.Value;
+            }
+            catch (OperationCanceledException)
+            {
+                return null;
+            }
+        }
+    }
+}
