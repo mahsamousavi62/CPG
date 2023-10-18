cba1c2493e5ea9f65134c3755707a7261991c243
Mahdokht-Kalantary <kalantarymahdokht@gmail.com>
2023-10-18 16:04:13 +0330

    Add cors origin

--- src/CPG.API/Program.cs
+++ src/CPG.API/Program.cs
@@ -21,6 +21,19 @@ builder.Services
 builder.Host.UseSerilog((context, configuation) =>
     configuation.ReadFrom.Configuration(context.Configuration));
 
+const string DefaultCorsPolicyName = "localhost";
+
+builder.Services.AddCors(
+                options => options.AddPolicy(
+                    DefaultCorsPolicyName,
+                    builder => builder
+                        .WithOrigins(configuration["CorsOrigins"]!
+                            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                            .ToArray()!)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed((host) => true)
+                        .AllowCredentials()));
 builder.Services.AddLocalization();
 builder.Services.Configure<RequestLocalizationOptions>(opt =>
 {
@@ -35,6 +48,8 @@ builder.Services.Configure<RequestLocalizationOptions>(opt =>
 });
 
 var app = builder.Build();
+
+app.UseCors(DefaultCorsPolicyName);
 
 if (app.Environment.IsDevelopment())
 {
