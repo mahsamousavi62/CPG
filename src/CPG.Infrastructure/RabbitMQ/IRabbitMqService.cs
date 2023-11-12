39e8606666ec1c56c9bab2f9d0fa086b6d00491b
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2023-11-12 13:19:46 +0330

    Add RabbitAppsetting ,Init Masstransit  and implement IUserRegisterConsumer in Infrastructure

---
+++ src/CPG.Infrastructure/RabbitMQ/IRabbitMqService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPG.Infrastructure.RabbitMQ
+{
+    public interface IRabbitMqService
+    {
+        void PublishMessage(string exchange, string routingKey, string message);
+
+        Task<string> ConsumeMessage(string queue);
+    }
+}
