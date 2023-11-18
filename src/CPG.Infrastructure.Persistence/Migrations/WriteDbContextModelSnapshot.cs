041fdad4d91f5475681f8c1a9699e3b93a04e65f
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2023-11-18 15:22:55 +0330

    fixed

--- src/CPG.Infrastructure.Persistence/Migrations/WriteDbContextModelSnapshot.cs
+++ src/CPG.Infrastructure.Persistence/Migrations/WriteDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace CPG.Infrastructure.Persistence.Migrations
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
+
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.13")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
@@ -287,7 +287,6 @@ namespace CPG.Infrastructure.Persistence.Migrations
                 {
                     b.Navigation("ActiveLoans");
                 });
-#pragma warning restore 612, 618
         }
     }
 }
