ef54bd7619a77af941b02c57a1c83fca3a4334c5
m.sepasi <m.sepasi@charisma.ir>
2023-11-28 10:12:04 +0330

    part2

---
+++ src/CPG.Application/UseCases/Providers/Commands/ActivateProvider/ActivateProviderCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPG.Application.UseCases.Providers.Commands.ActivateProvider
+{
+    internal class ActivateProviderCommandHandler
+    {
+    }
+}
