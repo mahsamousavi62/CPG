2e1b5329ef5ca55cd749703c3d803763b92bc916
m.sepasi <m.sepasi@charisma.ir>
2023-12-02 10:01:40 +0330

    part1

---
+++ src/CPG.Domain/AggregateModels/ApplicationAggregate/Exceptions/ApplicationIsNotActiveException.cs
@@ -0,0 +1,12 @@
+﻿using CPG.Domain.Exceptions;
+
+namespace CPG.Domain.AggregateModels.ApplicationAggregate.Exceptions;
+
+public class ApplicationIsNotActiveException : DomainException
+{
+    public override string Code => "application_is_already_not_active";
+    public long ApplicationId { get; }
+
+    public ApplicationIsNotActiveException(long applicationId) : base(string.Format(Resource.ApplicationIsAlreadyNotActive, applicationId))
+       => ApplicationId = applicationId;
+}
