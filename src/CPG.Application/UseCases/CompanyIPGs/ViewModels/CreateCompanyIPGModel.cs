fb47b37329d9064a328a1ba256d49b8e94d95e8e
m.sepasi <m.sepasi@charisma.ir>
2023-12-10 15:11:39 +0330

    add controller

---
+++ src/CPG.Application/UseCases/CompanyIPGs/ViewModels/CreateCompanyIPGModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPG.Application.UseCases.CompanyIPGs.ViewModels;
+
+public class CreateCompanyIPGModel
+{
+    [Required]
+    public long CompanyId { get; set; }
+
+    [Required]
+    public long ProviderId { get; set; }
+
+    [Required]
+    public long IPGTypeId { get; set; }
+
+    [Required]
+    public string ProviderData { get; set; }
+
+    [Required]
+    public short VerificationTimeLimit { get; set; }
+
+    [Required]
+    public CompanyIPGDepositViewModel[] CompanyIPGDeposits { get; set; }
+}
