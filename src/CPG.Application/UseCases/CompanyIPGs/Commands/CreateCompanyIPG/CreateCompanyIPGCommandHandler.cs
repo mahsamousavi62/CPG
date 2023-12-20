f03d7cba1ea0582437c7601d331d1a7fe435492e
m.sepasi <m.sepasi@charisma.ir>
2023-12-20 13:14:51 +0330

    add deposit existance check

--- src/CPG.Application/UseCases/CompanyIPGs/Commands/CreateCompanyIPG/CreateCompanyIPGCommandHandler.cs
+++ src/CPG.Application/UseCases/CompanyIPGs/Commands/CreateCompanyIPG/CreateCompanyIPGCommandHandler.cs
@@ -20,6 +20,11 @@ public class CreateCompanyIPGCommandHandler(IAggregateRepository<CompanyIPG> com
     {
         var depositsIds = request.Model.CompanyIPGDeposits.Select(t => t.DepositId).ToList();
         var deposits = await _depositRepository.ListAsync(new CompanyDepositsByIdList(depositsIds));
+        var notFoundDeposits = depositsIds.Where(t => !deposits.Select(d => d.Id).Contains(t));
+        if (notFoundDeposits?.Any() is true)
+        {
+            throw new NotFoundDepositException(string.Join(',', notFoundDeposits));
+        }
         var invalidDeposits = deposits.Where(t => t.CompanyId != request.Model.CompanyId).Select(t => t.Id);
         if (invalidDeposits?.Any() is true)
         {
