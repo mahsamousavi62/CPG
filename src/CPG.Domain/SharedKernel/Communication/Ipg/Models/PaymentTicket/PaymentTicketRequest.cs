9f877fad2433624b52b48d8f15a11a6fb44da716
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2023-12-23 09:54:24 +0330

    .

--- src/CPG.Domain/SharedKernel/Communication/Ipg/Models/PaymentTicket/PaymentTicketRequest.cs
+++ src/CPG.Domain/SharedKernel/Communication/Ipg/Models/PaymentTicket/PaymentTicketRequest.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
+﻿
 namespace CPG.Domain.SharedKernel.Communication.Ipg.Models.PaymentTicket
 {
     public class PaymentTicketRequest
     {
         public long CompanyIPGId { get; set; }
         public long PaymentRequestId { get; set; }
+        public short CompanyPaymentMethodType { get; set; }
     }
 }
