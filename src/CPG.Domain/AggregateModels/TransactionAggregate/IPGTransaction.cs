22bf31d96c46676a88eb920e2b12ea3ea3af8f0f
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2023-12-24 16:29:01 +0330

    .

---
+++ src/CPG.Domain/AggregateModels/TransactionAggregate/IPGTransaction.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using CPG.Domain.SeedWork;
+
+namespace CPG.Domain.AggregateModels.TransactionAggregate;
+public class IPGTransaction : AuditableEntity<long>
+{
+    public IPGTransaction(long paymentRquestId, string trackId, short status, bool isVerified, long companyIPGId, string iPGToken, string providerTrackerId, string referenceNumber, string encryptCardNumber, int verificationTimeLimit, DateTime predicateDateTime)
+    {
+        PaymentRquestId = paymentRquestId;
+        TrackId = trackId;
+        Status = status;
+        IsVerified = isVerified;
+        CompanyIPGId = companyIPGId;
+        IPGToken = iPGToken;
+        ProviderTrackerId = providerTrackerId;
+        ReferenceNumber = referenceNumber;
+        EncryptCardNumber = encryptCardNumber;
+        VerificationTimeLimit = verificationTimeLimit;
+        PredicateDateTime = predicateDateTime;
+    }
+    public string TrackId { get; set; }
+    public short Status { get; set; }
+    public bool IsVerified { get; set; }
+    public long CompanyIPGId { get; set; }
+    public string IPGToken { get; set; }
+    public string ProviderTrackerId { get; set; }
+    public string ReferenceNumber { get; set; }
+    public string EncryptCardNumber { get; set; }
+    public int VerificationTimeLimit { get; set; }
+    public DateTime PredicateDateTime { get; set; }
+}
+
---
+++ src/CPG.Domain/AggregateModels/TransactionAggregate/Transaction.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using CPG.Domain.SeedWork;
+
+namespace CPG.Domain.AggregateModels.TransactionAggregate;
+
+public class Transaction : AuditableEntity<long>, IAggregateRoot
+{
+    
+    public long PaymentRquestId { get; set; }
+
+    public Transaction(long paymentRquestId, long iPGTransactionId, short transactionMethodType, long companyId,
+                       long destinationDepositId, decimal amount, long applicationId, DateTime predictedSettlementDateTime)
+    {
+        PaymentRquestId = paymentRquestId;
+        IPGTransactionId = iPGTransactionId;
+        TransactionMethodType = transactionMethodType;
+        CompanyId = companyId;
+        DestinationDepositId = destinationDepositId;
+        Amount = amount;
+        ApplicationId = applicationId;
+        PredictedSettlementDateTime = predictedSettlementDateTime;
+    }
+
+    public long IPGTransactionId { get; set; }
+    public short TransactionMethodType { get; set; }
+    public long CompanyId { get; set; }
+    public long DestinationDepositId { get; set; }
+    public decimal Amount { get; set; }
+    public long ApplicationId { get; set; }
+    public DateTime PredictedSettlementDateTime { get; set; }
+    public short Status { get; set; }
+    public PaymentRequest PaymentRequest { get; set; }
+    public IPGTransaction IPGTransaction { get; set; }
+
+}
