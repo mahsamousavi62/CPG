61b3793fc518dae9d0e8f68048f9dbb1f52cbb82
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2023-12-24 14:43:54 +0330

    .

---
+++ src/CPG.Domain/AggregateModels/PaymentRequestAggregate/IPGTransaction.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using CPG.Domain.SeedWork;
+
+namespace CPG.Domain.AggregateModels.PaymentRequestAggregate;
+public class IPGTransaction : AuditableEntity<long>
+{
+    public long PaymentRquestId { get; set; }
+    public string TrackId { get; set; }
+    public short Status { get; set; }
+    public bool IsVerified { get; set; }
+    public long CompanyIPGId { get; set; }
+    public string IPGToken { get; set; }
+    public string ProviderTrackerId { get; set; }
+    public string ReferenceNumber { get; set; }
+    public string EncryptCardNumber { get; set; }
+    public int VerificationTimeLimit { get; set; }
+    public DateTime PredicateDateTime { get; set; }
+    public PaymentRequest PaymentRequest { get; set; }
+}
+
---
+++ src/CPG.Domain/AggregateModels/PaymentRequestAggregate/Transaction.cs
@@ -0,0 +1,12 @@
+﻿
+using CPG.Domain.SeedWork;
+
+namespace CPG.Domain.AggregateModels.PaymentRequestAggregate;
+
+public class Transaction : AuditableEntity<long>
+{
+    public long PaymentRquestId { get; set; }
+    public int MyProperty { get; set; }
+    public PaymentRequest PaymentRequest { get; set; }
+
+}
