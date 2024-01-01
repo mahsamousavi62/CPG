9cef04e6aaf3fc297fb4f3e96cde9854a719cf06
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2024-01-01 15:47:44 +0330

    asanpardakhtGetToken

--- src/CPG.Domain/AggregateModels/TransactionAggregate/IPGTransaction.cs
+++ src/CPG.Domain/AggregateModels/TransactionAggregate/IPGTransaction.cs
@@ -24,8 +24,8 @@ public class IPGTransaction : AuditableEntity<long>
     public string ReferenceNumber { get; set; }
     public string EncryptCardNumber { get; set; }
     public int VerificationTimeLimit { get; set; }
-    public DateTime VerificationDateTime { get; set; }
-    public DateTime PredicateDateTime { get; set; }
+    public DateTime? VerificationDateTime { get; set; }
+    public DateTime? PredicateDateTime { get; set; }
     public Transaction Transaction { get; set; }
 
     public static IPGTransaction Create(string trackerId, Enums.IPGTransactionStatus status, long companyIpgId,
