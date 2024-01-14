f8e9acd171a74f37d19539578620dd883668f131
SITEB\a.kianoor <ask1366@yahoo.com>
2024-01-14 15:07:10 +0330

    #FixCode

--- src/CPG.Infrastructure.Authorization/AuthenticationMiddleware.cs
+++ src/CPG.Infrastructure.Authorization/AuthenticationMiddleware.cs
@@ -1,69 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using CPG.Application.UseCases.Users.Queries;
-using CPG.Domain.AggregateModels.ApplicationAggregate;
-using CPG.Domain.AggregateModels.UserAggregate;
-using CPG.Domain.SharedKernel;
+﻿using CPG.Application.UseCases.Users.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using IAuthenticationService = CPG.Domain.SharedKernel.IAuthenticationService;
-namespace CPG.Infrastructure.Authorization
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CPG.Infrastructure.Authorization;
+
+public class AuthenticationMiddleware(IAuthenticationSchemeProvider schemes, RequestDelegate next)
 {
-    public class AuthenticationMiddleware
+    private readonly RequestDelegate _next = next ?? throw new ArgumentNullException(nameof(next));
+
+    public IAuthenticationSchemeProvider Schemes { get; set; } = schemes ?? throw new ArgumentNullException(nameof(schemes));
+
+    public async Task Invoke(HttpContext context, IMediator mediator)
     {
-        private readonly RequestDelegate _next;
-
-        public AuthenticationMiddleware(IAuthenticationSchemeProvider schemes, RequestDelegate next)
+        if (context.User.Identity.IsAuthenticated)
         {
-            Schemes = schemes ?? throw new ArgumentNullException(nameof(schemes));
-            _next = next ?? throw new ArgumentNullException(nameof(next));
-        }
-
-        public IAuthenticationSchemeProvider Schemes { get; set; }
-
-        public async Task Invoke(HttpContext context, IMediator mediator)
-        {
-            if (context.User.Identity.IsAuthenticated)
+            var defaultAuthenticate = await Schemes.GetDefaultAuthenticateSchemeAsync();
+            if (defaultAuthenticate != null)
             {
-                var defaultAuthenticate = await Schemes.GetDefaultAuthenticateSchemeAsync();
-                if (defaultAuthenticate != null)
-                {
-                    var result = await context.AuthenticateAsync(defaultAuthenticate.Name);
-                    if (result?.Principal != null)
-                        context.User = await ClonePrincipal(result.Principal,mediator);
-                }
+                var result = await context.AuthenticateAsync(defaultAuthenticate.Name);
+                if (result?.Principal != null)
+                    context.User = await ClonePrincipal(result.Principal, mediator);
             }
-            await _next(context);
         }
+        await _next(context);
+    }
 
 
-        public async Task<ClaimsPrincipal> ClonePrincipal(ClaimsPrincipal principal,IMediator mediator)
-        {
-            var clone = principal.Clone();
-            var newIdentity = (ClaimsIdentity)clone.Identity;
-           
-            var user = await mediator.Send(new GetUserAuthenticateQuery());
-            if (user == null)
-                return principal;
-          
-            newIdentity.AddClaim(new Claim(ClaimTypes.Name, user?.FirstName, ClaimValueTypes.String));
-            newIdentity.AddClaim(new Claim(ClaimTypes.Surname, user?.LastName, ClaimValueTypes.String));
-            newIdentity.AddClaim(new Claim(ClaimTypes.Sid, user?.IDPId, ClaimValueTypes.String));
-            newIdentity.AddClaim(new Claim(ClaimTypes.MobilePhone, user?.PhoneNumber, ClaimValueTypes.String));
-            newIdentity.AddClaim(new Claim(type: "NationalCode", value: user?.NationalCode));
-            newIdentity.AddClaim(new Claim(type: "CompanyId", value: user?.CompanyId.ToString()));
-            newIdentity.AddClaim(new Claim(type: "UserId", value: user?.Id.ToString()));
-            newIdentity.AddClaim(new Claim(type: "AuditType", value: user?.AuditType.ToString()));
-            newIdentity.AddClaim(new Claim(type: "ApplicationId", value: user?.ApplicationId.ToString()));
-            newIdentity.AddClaim(new Claim(type: "ClientId", value: user?.IDPId.ToString()));
+    private static async Task<ClaimsPrincipal> ClonePrincipal(ClaimsPrincipal principal, IMediator mediator)
+    {
+        var clone = principal.Clone();
+        var newIdentity = (ClaimsIdentity)clone.Identity;
 
-            return clone;
-        }
+        var user = await mediator.Send(new GetUserAuthenticateQuery());
+        if (user == null)
+            return principal;
+
+        newIdentity.AddClaim(new Claim(ClaimTypes.Name, user?.FirstName, ClaimValueTypes.String));
+        newIdentity.AddClaim(new Claim(ClaimTypes.Surname, user?.LastName, ClaimValueTypes.String));
+        newIdentity.AddClaim(new Claim(ClaimTypes.Sid, user?.IDPId, ClaimValueTypes.String));
+        newIdentity.AddClaim(new Claim(ClaimTypes.MobilePhone, user?.PhoneNumber, ClaimValueTypes.String));
+        newIdentity.AddClaim(new Claim(type: "NationalCode", value: user?.NationalCode));
+        newIdentity.AddClaim(new Claim(type: "CompanyId", value: user?.CompanyId.ToString()));
+        newIdentity.AddClaim(new Claim(type: "UserId", value: user?.Id.ToString()));
+        newIdentity.AddClaim(new Claim(type: "AuditType", value: user?.AuditType.ToString()));
+        newIdentity.AddClaim(new Claim(type: "ApplicationId", value: user?.ApplicationId.ToString()));
+        newIdentity.AddClaim(new Claim(type: "ClientId", value: user?.IDPId.ToString()));
+
+        return clone;
     }
 }
