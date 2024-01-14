922c41d63992c5c793816fb834191f70b5fb52b0
m.sepasi <m.sepasi@charisma.ir>
2024-01-14 19:43:58 +0330

    #18868

---
+++ src/CPG.Domain/SharedKernel/Communication/Ipg/Models/Verify/SepVerifyTransactionRequest.cs
@@ -0,0 +1,13 @@
+﻿using CPG.Domain.SharedKernel.Communication.Ipg.Sep;
+using System.Text.Json.Serialization;
+
+namespace CPG.Domain.SharedKernel.Communication.Ipg.Models.Verify;
+
+public class SepVerifyTransactionRequest : SepRequestBase
+{
+    [JsonPropertyName("terminalNumber")]
+    public long TerminalNumber { get; set; }
+
+    [JsonPropertyName("refNum")]
+    public string RefNum { get; set; }
+}
