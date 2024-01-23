eb2c80f0b8eba17ef582b48ed68d5dd047d6cbe7
m.sepasi <m.sepasi@charisma.ir>
2024-01-23 11:04:18 +0330

    Graphql

---
+++ src/CPG.Infrastructure.Persistence/GraphQL/Types/Bank/BankFilterType.cs
@@ -0,0 +1,18 @@
+﻿using CPG.Infrastructure.Persistence.DbContexts.ReadModels;
+using CPG.Infrastructure.Persistence.GraphQL.CustomFilterInputType;
+using CPG.Infrastructure.Persistence.GraphQL.FilterInputType;
+using HotChocolate.Data.Filters;
+
+namespace CPG.Infrastructure.Persistence.GraphQL.Types.Bank;
+
+internal class BankFilterType : FilterInputType<BankReadModel>
+{
+    protected override void Configure(
+   IFilterInputTypeDescriptor<BankReadModel> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+        descriptor.Field(f => f.Name).Type<CustomStringOperationFilterInputType>();
+        descriptor.Field(f => f.IsActive).Type<CustomBooleanOperationFilterInputType>();
+        descriptor.Field(f => f.Id).Type<CustomLongOperationFilterInputType>();
+    }
+}
---
+++ src/CPG.Infrastructure.Persistence/GraphQL/Types/Company/CompanyFilterType.cs
@@ -0,0 +1,19 @@
+﻿using CPG.Infrastructure.Persistence.DbContexts.ReadModels;
+using CPG.Infrastructure.Persistence.GraphQL.CustomFilterInputType;
+using CPG.Infrastructure.Persistence.GraphQL.FilterInputType;
+using HotChocolate.Data.Filters;
+
+namespace CPG.Infrastructure.Persistence.GraphQL.Types.Company;
+
+public class CompanyFilterType : FilterInputType<CompanyReadModel>
+{
+    protected override void Configure(
+   IFilterInputTypeDescriptor<CompanyReadModel> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+        descriptor.Field(f => f.PersianName).Type<CustomStringOperationFilterInputType>();
+        descriptor.Field(f => f.EnglishName).Type<CustomStringOperationFilterInputType>();
+        descriptor.Field(f => f.IsActive).Type<CustomBooleanOperationFilterInputType>();
+        descriptor.Field(f => f.Id).Type<CustomLongOperationFilterInputType>();
+    }
+}
