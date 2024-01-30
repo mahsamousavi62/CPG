16f844accecf80c80f3158123160c119c9ae1458
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2024-01-30 15:46:42 +0330

    companydeposit

--- src/CPG.Domain/AggregateModels/CompanyDepositAggregate/CompanyDeposit.cs
+++ src/CPG.Domain/AggregateModels/CompanyDepositAggregate/CompanyDeposit.cs
@@ -13,24 +13,16 @@ namespace CPG.Application.UseCases.CompanyDeposits;
 
 public class CompanyDeposit : AuditableEntity<long>, IAggregateRoot
 {
-    public string Name { get; }
-
-    public int BankId { get; }
-
-    public string Iban { get; }
-
-    public string AccountNumber { get; }
-
-    public long CompanyId { get; }
-
+    public string Name { get; set; }
+    public int BankId { get; set; }
+    public string Iban { get; set; }
+    public string AccountNumber { get; set; }
+    public long CompanyId { get; set; }
     public bool? IsDefaultForDD { get; set; }
 
     public Company Company { get; set; }
-
     public Bank Bank { get; set; }
-
     public List<CompanyIPGDeposit> CompanyIPGDeposits { get; set; }
-
     public List<Transaction> Transactions { get; set; }
 
     public CompanyDeposit()
@@ -62,6 +54,6 @@ public class CompanyDeposit : AuditableEntity<long>, IAggregateRoot
 
     public static void Update(CompanyDeposit companyDeposit, PersianName persianName)
     {
-        throw new NotImplementedException();
+       companyDeposit.Name=persianName.Value;
     }
 }
