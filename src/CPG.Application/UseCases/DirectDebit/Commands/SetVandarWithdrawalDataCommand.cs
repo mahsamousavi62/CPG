d9e4f39fba473f5c111468b6559a13dc0a765e98
m.sepasi <m.sepasi@charisma.ir>
2024-02-27 17:26:54 +0330

    #18987 #18985

---
+++ src/CPG.Application/UseCases/DirectDebit/Commands/SetVandarWithdrawalDataCommand.cs
@@ -0,0 +1,10 @@
+﻿using CPG.Domain.SharedKernel;
+using CPG.Domain.SharedKernel.Communication.DirectDebit.Vandar.WebHook;
+using MediatR;
+
+namespace CPG.Application.UseCases.DirectDebit.Commands;
+
+public class SetVandarWithdrawalDataCommand(WithdrawalWebhookRequest model) : IRequest<Result<bool>>
+{
+    public WithdrawalWebhookRequest model { get; set; } = model;
+}
