cc3c3684b8032a8143be844c6b7bccd9e6dc8877
m.sepasi <m.sepasi@charisma.ir>
2024-03-02 15:13:50 +0330

    remove directdebit from GetPaymentTokenQuery

--- src/CPG.Application/UseCases/Ipg/ViewModels/PaymentTokenViewModel.cs
+++ src/CPG.Application/UseCases/Ipg/ViewModels/PaymentTokenViewModel.cs
@@ -2,8 +2,7 @@
 
 public class PaymentTokenViewModel
 {
-    public long? CompanyIPGId { get; set; }
+    public long CompanyIPGId { get; set; }
     public string PaymentRequestCode { get; set; }
-    public short? CompanyPaymentMethodType { get; set; }
-    public long? GrantId { get; set; }
+    public short CompanyPaymentMethodType { get; set; }
 }
