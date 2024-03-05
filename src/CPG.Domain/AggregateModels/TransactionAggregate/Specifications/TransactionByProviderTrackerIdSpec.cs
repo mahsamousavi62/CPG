1400403753c1a76a49693dfeb47ffd7815861244
m.sepasi <m.sepasi@charisma.ir>
2024-03-05 17:18:35 +0330

    fix webHook

---
+++ src/CPG.Domain/AggregateModels/TransactionAggregate/Specifications/TransactionByProviderTrackerIdSpec.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using System.Linq;
+
+namespace CPG.Domain.AggregateModels.TransactionAggregate.Specifications;
+
+public class TransactionByProviderTrackerIdSpec : Specification<Transaction>, ISingleResultSpecification<Transaction>
+{
+    public TransactionByProviderTrackerIdSpec(string providerTrackerId)
+    {
+        Query.Include(t => t.DirectDebitTransaction)
+            .Include(t => t.PaymentRequest)
+            .Where(a => a.DirectDebitTransaction.ProviderTrackerId == providerTrackerId && a.PaymentRequest.IsActive);
+    }
+}
