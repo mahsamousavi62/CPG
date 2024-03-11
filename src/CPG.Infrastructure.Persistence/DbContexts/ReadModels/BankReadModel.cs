2aa04fe5006ae3bcd76d924b6bf8988e9763a103
m.mousavi <m.mousavi@NC-M-MOUSAVII>
2024-03-11 11:05:19 +0330

    fix logoaddress bank

--- src/CPG.Infrastructure.Persistence/DbContexts/ReadModels/BankReadModel.cs
+++ src/CPG.Infrastructure.Persistence/DbContexts/ReadModels/BankReadModel.cs
@@ -6,15 +6,27 @@ namespace CPG.Infrastructure.Persistence.DbContexts.ReadModels;
 
 public class BankReadModel
 {
+    private string _logoAddress;
+    public string LogoAddress
+    {
+        get => _logoAddress;
+        set
+        {
+            _logoAddress = value;
+
+            Logo = _logoAddress;
+        }
+    }
     public int Id { get; set; }
     public string Name { get; set; }
     public string IbanPrefix { get; set; }
     public bool IsActive { get; set; }
-    public string LogoAddress { get; set; }
+
     public bool? HasDirectDebitFeature { get; set; }
     public DateTime CreationDate { get; set; }
     public DateTime? ModificationDate { get; set; }
     public ICollection<CompanyDepositReadModel> CompanyDeposits { get; set; }
     public ICollection<DirectDebitGrantReadModel> DirectDebitGrants { get; set; }
     public BankDirectDebitSettingReadModel? DirectDebitSetting { get; set; }
+    public string Logo { get; private set; }
 }
