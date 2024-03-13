2b176c31bfa12f4810b402588e322a6ac6bce4d5
Mahdokht-Kalantary <kalantarymahdokht@gmail.com>
2024-03-13 13:25:30 +0330

    fix charismacart

--- src/CPG.Application/UseCases/PaymentRequests/Commands/GetPaymentMethods/GetPaymentMethodsCommandHandler.cs
+++ src/CPG.Application/UseCases/PaymentRequests/Commands/GetPaymentMethods/GetPaymentMethodsCommandHandler.cs
@@ -121,17 +121,17 @@ public class GetPaymentMethodsCommandHandler(IAggregateRepository<PaymentRequest
                 {
                     var userDepositBalance = await _neoBankService.GetUserDepositBalance();
 
-                    charismaCard = userDepositBalance.Data.DepositStatus switch
+                    if (userDepositBalance?.Data is not null)
                     {
-                        _ => new ViewModels.CharismaCard
+                        charismaCard = new ViewModels.CharismaCard
                         {
                             BalanceAmount = userDepositBalance.Data.Balance,
                             CardNumber = userDepositBalance.Data.CardNumber,
                             CustomerSurname = $"{userDepositBalance.Data.CustomerFirstName} {userDepositBalance.Data.CustomerLastName}",
                             DepositStatus = userDepositBalance.Data.DepositStatus,
                             ExpirationDate = userDepositBalance.Data.ExpirationDate
-                        }
-                    };
+                        };
+                    }
                 }
             }
             else
