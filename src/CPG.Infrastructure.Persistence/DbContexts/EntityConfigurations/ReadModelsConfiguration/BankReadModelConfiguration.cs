e175d6a3cb4718b3a81ae883e9318558cac3b016
m.sepasi <m.sepasi@charisma.ir>
2024-03-13 11:22:50 +0330

    change bank logoAddress property name

--- src/CPG.Infrastructure.Persistence/DbContexts/EntityConfigurations/ReadModelsConfiguration/BankReadModelConfiguration.cs
+++ src/CPG.Infrastructure.Persistence/DbContexts/EntityConfigurations/ReadModelsConfiguration/BankReadModelConfiguration.cs
@@ -10,11 +10,11 @@ public class BankReadModelConfiguration : IEntityTypeConfiguration<BankReadModel
     {
         readModel.ToTable("Bank");
         readModel.HasKey(x => x.Id);
-        readModel.Property(x => x.Id).HasColumnName("Id");
-        readModel.Property(x => x.Name).HasColumnName("Name");
-        readModel.Property(x => x.IbanPrefix).HasColumnName("IbanPrefix");
-        readModel.Property(x => x.Logo).HasColumnName("LogoAddress");
-        readModel.Property(x => x.HasDirectDebitFeature).HasColumnName("HasDirectDebitFeature");
+        readModel.Property(x => x.Id);
+        readModel.Property(x => x.Name);
+        readModel.Property(x => x.IbanPrefix);
+        readModel.Property(x => x.Logo);
+        readModel.Property(x => x.HasDirectDebitFeature);
         readModel.Property(x => x.IsActive);
         readModel.Property(x => x.CreationDate);
         readModel.Property(x => x.ModificationDate);
