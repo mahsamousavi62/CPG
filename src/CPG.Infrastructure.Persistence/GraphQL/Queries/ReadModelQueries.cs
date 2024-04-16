4baeb97066ecb58c68f7645bf29938014ec2edca
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2024-04-16 11:10:19 +0330

    fix report

--- src/CPG.Infrastructure.Persistence/GraphQL/Queries/ReadModelQueries.cs
+++ src/CPG.Infrastructure.Persistence/GraphQL/Queries/ReadModelQueries.cs
@@ -383,7 +383,7 @@ public class ReadModelQueries
 
              data.Select(async entity => new IpgTransactionReportViewModel
              {
-                 TotalCount = totalCount,
+                 IPGToken=entity.IPGTransaction.IPGToken,
                  Id = entity.Transaction.Id,
                  CompanyId = entity.Company.Id,
                  CompanyPersianName = entity.Company.PersianName,
@@ -397,6 +397,7 @@ public class ReadModelQueries
                  CreationDate = entity.IPGTransaction.CreationDate,
                  VerificationDateTime = entity.IPGTransaction.VerificationDateTime,
                  PredicateExpirationDateTime = entity.IPGTransaction.PredicateExpirationDateTime,
+                 PredictedSettlementDateTime = entity.Transaction.PredictedSettlementDateTime,
                  ReferenceNumber = entity.IPGTransaction?.ReferenceNumber,
                  TransactionStatusName = General.GetIPGTransactionStatusName(entity.IPGTransaction.Status),
                  TransactionStatus = entity.IPGTransaction.Status,
