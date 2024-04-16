4f94e5793683f13aafaab114241ec972348ee209
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2024-04-16 09:19:40 +0330

    #19290

---
+++ src/CPG.Infrastructure.Persistence/Migrations/20240415102518_AddApplicationSetting.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CPG.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationSetting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[Application_Settings]
+                                         ([Entity_Type],[Key],[Value],[CreationDate],[CreationUserId],[IsActive])
+                                  VALUES (7,'Referrer_Page','referrer-cpg',GETDATE(),1,1),
+                                         (7,'Callback_Page','callback-cpg',GETDATE(),1,1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
