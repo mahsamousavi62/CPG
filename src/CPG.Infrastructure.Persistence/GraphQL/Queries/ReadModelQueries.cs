ff5b3a0cab3d870090c5bccc5bf6a4c21a4021a4
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2024-04-16 11:19:15 +0330

    .

--- src/CPG.Infrastructure.Persistence/GraphQL/Queries/ReadModelQueries.cs
+++ src/CPG.Infrastructure.Persistence/GraphQL/Queries/ReadModelQueries.cs
@@ -654,6 +654,10 @@ public class ReadModelQueries
                  TransactionStatusName = General.GetCharismaCardTransactionStatusName(entity.CharismaCardTransaction.Status),
                  TransactionStatus = entity.CharismaCardTransaction.Status,
                  TransactionStatusCode = entity.CharismaCardTransaction.Status.GetValue(),
+                 ProviderTrackerId=entity.CharismaCardTransaction.ProviderTrackId,
+                 TrackerId=entity.CharismaCardTransaction.TrackId,
+                 ModificationDate = entity.CharismaCardTransaction.ModificationDate,
+
              }).ToList()
             );
 
