8c16999cf129a8c73107306c2203514db3fc9215
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2024-04-20 12:01:37 +0330

    .

--- src/CPG.Infrastructure.Persistence/DbContexts/ReadModels/TransactionReadModel.cs
+++ src/CPG.Infrastructure.Persistence/DbContexts/ReadModels/TransactionReadModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using CPG.Application.UseCases.PaymentRequests.ViewModels;
+using CPG.Domain.SeedWork;
 using CPG.Domain.SharedKernel;
 using CPG.Infrastructure.Persistence.DbContexts.ReadModels;
+using CPG.Infrastructure.Persistence.GraphQL.Model;
+using Mapster;
 
 namespace CPG.Domain.AggregateModels.TransactionAggregate;
 
-public class TransactionReadModel 
+public class TransactionReadModel
 {
     public long Id { get; set; }
     public long PaymentRquestId { get; set; }
@@ -29,5 +33,51 @@ public class TransactionReadModel
     public CharismaCardTransactionReadModel CharismaCardTransaction { get; set; }
     public CompanyDepositReadModel DestinationDeposit { get; set; }
     public CompanyReadModel Company { get; set; }
-    
+    public ApplicationReadModel Application { get; set; }
+
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<TransactionReadModel, TransactionReportViewModel>();
+    }
+}
+public static class MappingConfig
+{
+    public static void Configure()
+    {
+        TypeAdapterConfig<TransactionReadModel, TransactionReportViewModel>.NewConfig()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.CompanyId, src => src.Company.Id)
+            .Map(dest => dest.CompanyPersianName, src => src.Company.PersianName)
+            .Map(dest => dest.CompanyEnglishName, src => src.Company.EnglishName)
+            .Map(dest => dest.Amount, src => src.Amount)
+            .Map(dest => dest.TransactionMethodType, src => src.TransactionMethodType)
+            .Map(dest => dest.TransactionMethodTypeName, src => General.GetTransactionMethodTypeName(src.TransactionMethodType))
+            .Map(dest => dest.IPGTypeId, src => src.IPGTransaction.CompanyIPG.IPGTypeId)
+            .Map(dest => dest.IpgTypeName, src => src.IPGTransaction.CompanyIPG.IPGType.PersianName)
+            .Map(dest => dest.ProviderId, src => src.IPGTransaction.CompanyIPG.ProviderId)
+            .Map(dest => dest.ProviderName, src => src.IPGTransaction.CompanyIPG.Provider.PersianName)
+            .Map(dest => dest.ApplicationName, src => src.PaymentRequest.Application.PersianName)
+            .Map(dest => dest.PaymentCode, src => src.PaymentRequest.PaymentCode)
+            .Map(dest => dest.CompanyDepositName, src => src.DestinationDeposit.Name)
+            .Map(dest => dest.CompanyDepositaccountNumber, src => src.DestinationDeposit.AccountNumber)
+            .Map(dest => dest.CompanyDepositIban, src => src.DestinationDeposit.Iban)
+
+            .Map(dest => dest.TransactionCreateDateTime, src => src.CreationDate)
+            .Map(dest => dest.TransactionModificationDateTime, src => src.ModificationDate)
+            .Map(dest => dest.ApplicationId, src => src.PaymentRequest.ApplicationId)
+            //.Map(dest => dest.ReferenceNumber, src => src.IPGTransaction?.ReferenceNumber ??
+            //                                          src.CharismaCardTransaction?.ReferenceNumber ??
+            //                                          src.PaymentReceiptTransaction?.ReferenceNumber ??
+            //                                          src.DirectDebitTransaction?.TrackId)
+            .Map(dest => dest.TransactionStatusName, src => General.GetTransactionStatusName(src.Status))
+            .Map(dest => dest.TransactionStatus, src => src.Status)
+            .Map(dest => dest.TransactionStatusCode, src => src.Status.GetValue())
+
+
+
+
+
+            ;
+
+    }
 }
---
+++ src/CPG.Infrastructure.Persistence/Mapping/MappingConfig.cs
@@ -0,0 +1,50 @@
+﻿
+namespace CPG.Infrastructure.Persistence.Mapping;
+
+using CPG.Domain.AggregateModels.TransactionAggregate;
+using CPG.Domain.SharedKernel;
+using CPG.Domain.SharedKernel.Minio;
+using CPG.Infrastructure.Persistence.GraphQL.Model;
+using Mapster;
+
+public static class MappingConfig
+{
+    public static void Configure()
+    {
+       
+            //TypeAdapterConfig<TransactionReadModel, TransactionReportViewModel>.NewConfig()
+            //.Map(dest => dest.TransactionCreateDateTime, src => src.CreationDate)
+            //.Map(dest => dest.TransactionModificationDateTime, src => src.ModificationDate)
+            //.Map(dest => dest.TransactionStatus, src => src.Status)
+            //.Map(dest => dest.TransactionStatusCode, src => src.Status.GetValue())
+            //.Map(dest => dest.TransactionMethodType, src => src.TransactionMethodType)
+            //.Map(dest => dest.TransactionMethodTypeName, src => General.GetTransactionMethodTypeName(src.TransactionMethodType))
+            //.Map(dest => dest.CompanyId, src => src.Company.Id)
+            //.Map(dest => dest.CompanyPersianName, src => src.Company.PersianName)
+            //.Map(dest => dest.CompanyEnglishName, src => src.Company.EnglishName)
+            //.Map(dest => dest.Amount, src => src.Amount)
+            //.Map(dest => dest.ApplicationId, src => src.ApplicationId)
+            //.Map(dest => dest.ApplicationName, src => src.Application.PersianName)
+            //.Map(dest => dest.ReferenceNumber, src => src.IPGTransaction?.ReferenceNumber ??
+            //                                       src.CharismaCardTransaction?.ReferenceNumber ??
+            //                                       src.PaymentReceiptTransaction?.ReferenceNumber ??
+            //                                       src.DirectDebitTransaction?.TrackId)
+            //.Map(dest => dest.TransactionStatusName, src => General.GetTransactionStatusName(src.Status))
+            //.Map(dest => dest.IPGTypeId, src => src.IPGTransaction.CompanyIPG.IPGType?.Id ?? 0)
+            //.Map(dest => dest.IpgTypeName, src => src.IPGType?.PersianName)
+            //.Map(dest => dest.ProviderId, src => src.Provider?.Id ?? 0)
+            //.Map(dest => dest.ProviderName, src => src.Provider?.PersianName)
+            //.Map(dest => dest.PaymentCode, src => src.PaymentRequest?.PaymentCode)
+            //.Map(dest => dest.CompanyDepositName, src => src.CompanyDeposit?.Name)
+            //.Map(dest => dest.CompanyDepositaccountNumber, src => src.CompanyDeposit?.AccountNumber)
+            //.Map(dest => dest.CompanyDepositIban, src => src.CompanyDeposit?.Iban)
+            //.Map(dest => dest.FirstName, src => UserscacheData.FirstOrDefault(c => c.Id == src.Transaction.CreationUserId)?.FirstName)
+            //.Map(dest => dest.LastName, src => UserscacheData.FirstOrDefault(c => c.Id == src.Transaction.CreationUserId)?.LastName)
+            //.Map(dest => dest.NationalCode, src => UserscacheData.FirstOrDefault(c => c.Id == src.Transaction.CreationUserId)?.NationalCode)
+            //.Map(dest => dest.CompanyLogo, async (src, dest) => !string.IsNullOrEmpty(src.Company.Logo) ? await General.GetLogo(minioProvider, src.Company.Logo) : null);
+
+        ;
+    }
+}
+
+
