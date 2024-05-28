bd9767b2bf66e814aeec614b70bf3a0dd4843f17
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2024-05-28 16:31:44 +0330

    #19093

--- src/CPG.Application/UseCases/CharismaCard/Commands/CreateCharismaCardTransactionCommandHandler.cs
+++ src/CPG.Application/UseCases/CharismaCard/Commands/CreateCharismaCardTransactionCommandHandler.cs
@@ -9,7 +9,6 @@ using CPG.Domain.AggregateModels.CompanyDepositAggregate.Specifications;
 using CPG.Domain.AggregateModels.PaymentRequestAggregate.Specifications;
 using CPG.Domain.AggregateModels.TransactionAggregate;
 using CPG.Domain.SharedKernel;
-using CPG.Domain.SharedKernel.Communication.DirectDebit.Models.Show;
 using CPG.Domain.SharedKernel.Communication.NeoBank;
 using CPG.Domain.SharedKernel.Communication.NeoBank.Models;
 using MediatR;
@@ -17,31 +16,32 @@ using CPG.Domain.SharedKernel.Helper;
 using static CPG.Domain.SharedKernel.Enums;
 using System.Linq;
 using System.Text.RegularExpressions;
-using CPG.Application.Shared.Resource;
-using CPG.Application.UseCases.PaymentReceipt.ViewModels;
 using CPG.Application.UseCases.CharismaCard.ViewModels;
+using CPG.Application.Shared.Resource;
 
 namespace CPG.Application.UseCases.CharismaCard.Commands;
 
-public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBankService,
-    IAggregateRepository<PaymentRequest> paymentRequestAggregateRepository,
+public class CreateCharismaCardTransactionCommandHandler(
+    INeoBankService neoBankService,
+    IAuthenticationService authenticationService,
     IAggregateRepository<Transaction> transactionRepository,
-    IAggregateRepository<Domain.AggregateModels.CompanyDepositAggregate.CompanyDeposit> companyDepositRepository,
-    IAggregateRepository<Domain.AggregateModels.CompanyAggregate.Company> companyRepository,
+    IAggregateRepository<PaymentRequest> paymentRequestAggregateRepository,
     IAggregateRepository<Domain.AggregateModels.BankAggregate.Bank> bankRepository,
+    IAggregateRepository<Domain.AggregateModels.CompanyAggregate.Company> companyRepository,
     IAggregateRepository<Domain.AggregateModels.ProviderAggregate.Provider> providerRepository,
-    IAuthenticationService authenticationService)
+    IAggregateRepository<Domain.AggregateModels.CompanyDepositAggregate.CompanyDeposit> companyDepositRepository
+    )
     : IRequestHandler<CreateCharismaCardTransactionCommand, Result<CharismaCardResponseViewModel>>
 {
     private readonly INeoBankService neoBankService = neoBankService;
-    private readonly IAggregateRepository<PaymentRequest> _paymentRequestRepository = paymentRequestAggregateRepository;
+    private readonly IAuthenticationService _authenticationService = authenticationService;
     private readonly IAggregateRepository<Transaction> _transactionRepository = transactionRepository;
-    private readonly IAggregateRepository<Domain.AggregateModels.CompanyAggregate.Company> _companyRepository = companyRepository;
     private readonly IAggregateRepository<Domain.AggregateModels.BankAggregate.Bank> _bankRepository = bankRepository;
+    private readonly IAggregateRepository<PaymentRequest> _paymentRequestRepository = paymentRequestAggregateRepository;
+    private readonly IAggregateRepository<Domain.AggregateModels.CompanyAggregate.Company> _companyRepository = companyRepository;
     private readonly IAggregateRepository<Domain.AggregateModels.ProviderAggregate.Provider> _providerRepository = providerRepository;
     private readonly IAggregateRepository<Domain.AggregateModels.CompanyDepositAggregate.CompanyDeposit> _companyDepositRepository = companyDepositRepository;
-    private readonly IAuthenticationService _authenticationService = authenticationService;
-    public async Task<Result<CharismaCardResponseViewModel>> Handle(CreateCharismaCardTransactionCommand request, 
+    public async Task<Result<CharismaCardResponseViewModel>> Handle(CreateCharismaCardTransactionCommand request,
         CancellationToken cancellationToken)
     {
         var paymentRequest = await _paymentRequestRepository.GetBySpecAsync(new PaymentRequestByCode(request.Model.PaymentCode), cancellationToken);
@@ -55,17 +55,9 @@ public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBank
 
         long destinationDepositId;
         Domain.AggregateModels.CompanyDepositAggregate.CompanyDeposit companyDeposit;
-        //if (!string.IsNullOrWhiteSpace(paymentRequest.DestinationDepositIban))
-        //{
-        //    companyDeposit = await _companyDepositRepository.GetBySpecAsync(new CompanyDepositByIban(paymentRequest.DestinationDepositIban), cancellationToken);
-        //    if (companyDeposit is null) throw new Exception("CompanyDeposit not found!");
-        //    if (companyDeposit.CompanyId != paymentRequest.CompanyId) { throw new PaymentTokenDepositNotBelongsCompanyException(); }
-        //}
-        //else
-        //{
-            companyDeposit = await _companyDepositRepository.GetBySpecAsync(new DefaultCharismaCardDepositSpec(paymentRequest.CompanyId), cancellationToken);
-            if (companyDeposit is null) throw new Exception("Default CompanyDeposit for charismCard not found!");
-        //}
+
+        companyDeposit = await _companyDepositRepository.GetBySpecAsync(new DefaultCharismaCardDepositSpec(paymentRequest.CompanyId), cancellationToken);
+        if (companyDeposit is null) throw new Exception("Default CompanyDeposit for charismCard not found!");
 
         if (!companyDeposit.IsActive) { throw new PaymentTokenInactiveDepositException(); }
         var bank = companyDeposit.Bank;
@@ -73,13 +65,12 @@ public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBank
         if (company.PaymentMethods?.Any(t => t.MethodType == Enums.PaymentMethodType.CharismaCard) is false)
         { throw new PaymentRequestCompanyHasNoCharismaCardMethodException(); }
 
-
         var mobileNumber = await _authenticationService.GetDataFromClaim<string>(ClaimTypes.MobilePhone);
         destinationDepositId = companyDeposit.Id;
-        
+
 
         var trackId = RandomGenerator.GenerateRandomDigitNumber(16);
-       var accountNumber = Regex.Replace(companyDeposit.AccountNumber, @"(\d{4})(\d{2})(\d{3})(\d+)", "$1/$2/$3/$4");
+        var accountNumber = Regex.Replace(companyDeposit.AccountNumber, @"(\d{4})(\d{2})(\d{3})(\d+)", "$1/$2/$3/$4");
         var clientDirectDebitResponse = await neoBankService.ClientDirectDebit(new ClientDirectDebitRequest
         {
             Amount = paymentRequest.Amount,
@@ -88,10 +79,16 @@ public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBank
             TrackerId = trackId,
         });
 
-        if (clientDirectDebitResponse?.IsSuccess == true && string.IsNullOrEmpty(clientDirectDebitResponse?.Data?.ErrorCode))
+        if (clientDirectDebitResponse?.IsSuccess == true)
         {
             ClientDirectDebitResponse clientDirectDebit = clientDirectDebitResponse.Data;
-
+            if (!string.IsNullOrEmpty(clientDirectDebitResponse?.Data?.ErrorCode))
+            {
+                if (clientDirectDebit.ErrorCode == "10")
+                {
+                    return Result<CharismaCardResponseViewModel>.Failure(new Error("2202005", string.Format(GlobalResource.DirectDebitGrantError, company.PersianName)));
+                }
+            }
             var CharismaCardstatus = clientDirectDebit.TransferStatus == NeoBankTransferStatus.Failed ?
                 CharismaCardStatus.Failed : CharismaCardStatus.Done;
             Transaction transaction = Transaction.Create(new CreateTransactionModel
@@ -99,7 +96,7 @@ public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBank
                 CharismaCardModel = new CharismaCardTransactionModel
                 {
                     TrackId = trackId,
-                    ProviderTrackId = clientDirectDebit.TranactionId??"0",
+                    ProviderTrackId = clientDirectDebit.TranactionId ?? "0",
                     ReferenceNumber = clientDirectDebit.ReferenceNumber,
                     Status = CharismaCardstatus
                 },
@@ -133,7 +130,7 @@ public class CreateCharismaCardTransactionCommandHandler(INeoBankService neoBank
             await _paymentRequestRepository.SaveChangesAsync();
             await _transactionRepository.UpdateAsync(transaction);
             await _transactionRepository.SaveChangesAsync();
-           
+
             return Result<CharismaCardResponseViewModel>.SuccessResult(new CharismaCardResponseViewModel
             {
                 CallBackUrl = $"{transaction.PaymentRequest.CallBackUrl}/paymentResult?paymentCode={transaction.PaymentRequest.PaymentCode}&paymentStatus={General.GetPaymentStatusTitle(transaction.PaymentRequest.Status)}"
