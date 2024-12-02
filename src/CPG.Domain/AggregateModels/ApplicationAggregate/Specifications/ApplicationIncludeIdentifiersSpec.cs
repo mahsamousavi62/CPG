a9f37f4863bd6af9d1f02cf6a161cdd749686e82
SITEB\a.kianoor <ask1366@yahoo.com>
2024-12-02 15:37:54 +0330

    FixBugsAndNaming

---
+++ src/CPG.Domain/AggregateModels/ApplicationAggregate/Specifications/ApplicationIncludeIdentifiersSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using System.Linq;
+
+namespace CPG.Domain.AggregateModels.ApplicationAggregate.Specifications;
+
+public class ApplicationIncludeIdentifiersSpec : Specification<Application>, ISingleResultSpecification<Application>
+{
+    public ApplicationIncludeIdentifiersSpec()
+    {
+        Query.Include(a=>a.ApplicationIdentifiers);
+    }
+}
