5d0c3e53130876f3f8e60752769df4eeb0d5218a
m.mousavi <m.mousavi@NC-M-MOUSAVI>
2025-04-09 10:26:12 +0330

    getUserQuery

--- src/CPG.Infrastructure.Persistence/QueryHandlers/Users/GetUserQueryHandler.cs
+++ src/CPG.Infrastructure.Persistence/QueryHandlers/Users/GetUserQueryHandler.cs
@@ -33,6 +33,11 @@ public class GetUserQueryHandler(
 
         var kycStatus = await _authenticationService.GetDataFromClaim<string>("status", string.Empty);
 
+        var user = await _context.UserReadModels
+    .Include(u => u.UserRoles)
+    .Include(c => c.Company)
+    .SingleOrDefaultAsync(u => u.IsActive && u.IDPId == sub, cancellationToken: cancellationToken);
+
         if (kycStatus != userKycStatus && kycStatus != demo)
         {
             var idpUserProfileResponse = await _idpClient.GetUserStatus(sub);
@@ -47,15 +52,16 @@ public class GetUserQueryHandler(
             var idpUserProfileResponse = await _idpClient.GetUserStatus(sub);
             if (idpUserProfileResponse.OperationResult is Enums.OperationResult.Succeeded)
             {
-                if (idpUserProfileResponse.Data?.Result?.Status == userKycStatus)
+                if (idpUserProfileResponse.Data?.Result?.Status == userKycStatus && user is null)
                     throw new UserNotFoundException(sub);
             }
         }
 
-        var user = await _context.UserReadModels
-            .Include(u => u.UserRoles)
-            .Include(c => c.Company)
-            .SingleOrDefaultAsync(u => u.IsActive && u.IDPId == sub, cancellationToken: cancellationToken);
+        if (user is null && kycStatus == userKycStatus)
+        {
+            throw new UserNotFoundException(sub);
+        }
+
 
         //if (user != null)
         //{
@@ -63,10 +69,6 @@ public class GetUserQueryHandler(
         //        .SingleOrDefaultAsync(a => a.IdpClientId == sub, cancellationToken: cancellationToken))?.ApplicationId;
         //}
 
-        if (user is null && kycStatus == userKycStatus)
-        {
-            throw new UserNotFoundException(sub);
-        }
 
         var userViewModel = new UserViewModel
         {
