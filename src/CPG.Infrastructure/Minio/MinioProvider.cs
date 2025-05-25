184fa58522453a193e7530f182d52357bb2b1eb4
SITEB\a.kianoor <ask1366@yahoo.com>
2025-05-25 13:27:30 +0330

    log response

--- src/CPG.Infrastructure/Minio/MinioProvider.cs
+++ src/CPG.Infrastructure/Minio/MinioProvider.cs
@@ -70,7 +70,7 @@ public class MinioProvider : IMinioProvider
 
             var response = await _minioClient.PutObjectAsync(putObjectArgs).ConfigureAwait(false);
 
-            var resString = JsonConvert.SerializeObject(putObjectArgs);
+            var resString = JsonConvert.SerializeObject(response);
 
             _logger.LogWarning($"Response Minio : {resString}");
 
